586f4771e84e53cc58a3417e6da5f6685c12a316
Junior Guerra <juniorguerra@github.com>
2024-05-01 21:19:59 -0500

    update(changelot): se cambia por chromewebdriver

--- src/scrapping/scrapping.py
+++ src/scrapping/scrapping.py
@@ -2,20 +2,18 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions 
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
+service = Service("/home/juno/Downloads/chromedriver-linux64/chromedriver")
 
-
-options = webdriver.FirefoxOptions()
-options.add_argument('--no-sandbox') 
-# options.add_argument("--headless")
-driver =  webdriver.Firefox(options=options)
-driver.get("https://procesosjudiciales.funcionjudicial.gob.ec/busqueda-filtros")
+driver = webdriver.Chrome(service=service)
 driver.implicitly_wait(time_to_wait=10)
-
+driver.get("https://procesosjudiciales.funcionjudicial.gob.ec/busqueda-filtros")
 
 kindOfDemand = ""
 if kindOfDemand == "":
@@ -63,11 +61,9 @@ for process in processes:
             print(general_info)
             print()
         except StaleElementReferenceException:
-            # Si se produce la excepción StaleElementReferenceException, volvemos a encontrar el icono de detalle
             icon_detail = process.find_element(By.CLASS_NAME, "search-icon")
             icon_detail.click()
             WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "search-icon")))
-            # Luego continuamos con el proceso de obtención de información
             general_info_elements = driver.find_elements(By.XPATH, "//section[@class='filtros-busqueda']//*")
             general_info = {}
 
@@ -81,13 +77,6 @@ for process in processes:
         
             print(general_info)
             print()
-
-
-
-
-
-
-
             
     #     processes_detail = driver.find_elements(By.CLASS_NAME, "movimiento-individual")
 
