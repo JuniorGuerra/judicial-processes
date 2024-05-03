d2baf21e5168c6869bfb5febe407a3ae71fbf390
Junior Guerra <juniorguerra@github.com>
2024-05-03 12:28:07 -0500

    update(scraping_test): cambio

--- src/scraping/scraping_test.py
+++ src/scraping/scraping_test.py
@@ -1,12 +1,12 @@
 import unittest
-import scrapping
+import scraping
 import concurrent.futures
 
 class TestScraping(unittest.TestCase):
     def test_scraping_with_concurrency(self):
         urls = ["https://procesosjudiciales.funcionjudicial.gob.ec/busqueda-filtros"] * 15 
         with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
-            future_to_url = {executor.submit(scrapping.get_page_info, "0968599020001", url, True, "/home/juno/Downloads/chrome-2/chromedriver-linux64/chromedriver"): url for url in urls}
+            future_to_url = {executor.submit(scraping.get_page_info, "0968599020001", url, True, "/home/juno/Downloads/chrome-2/chromedriver-linux64/chromedriver"): url for url in urls}
             for future in concurrent.futures.as_completed(future_to_url):
                 self.assertTrue(future.done())
 
