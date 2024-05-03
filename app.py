10667d53e0da9ce6c14ae6f6ed96f11dd30da3bb
Junior Guerra <juniorguerra@github.com>
2024-05-02 23:53:50 -0500

    update(heroku-sucess): sucessfull heroku devops

--- app.py
+++ app.py
@@ -81,9 +81,6 @@ def get_document(document_id):
     return jsonify(response)
 
 
-@app.route("/")
-def hello_world():
-    return "<h1 style='color:green'>Hello World!</h1>"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
